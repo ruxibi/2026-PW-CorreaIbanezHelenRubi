8273353909a5e124795c38c0d853f1d0e4f9ee58
ruxibi <hcorreai2400@alumno.ipn.mx>
2026-09-23 02:30:20 -0600

    app121

--- Primer parcial/02Ejercicios/js/app.js
+++ Primer parcial/02Ejercicios/js/app.js
@@ -11,35 +11,23 @@ const talleres = [
 function pintarTabla(){
     //debe de obtener la tabla y rellenarla con los datos de talleres
     const tabla = document.getElementById('tabla-talleres');
-    const tbody = tabla.querySelector('tbody');
+    tabla.innerHTML="";
 
     talleres.forEach((taller) => {
 
         const fila = document.createElement('tr');
+        fila.innerHTML=
+        `
+        <td>$(taller.nombre)</td>
+        <td>$(taller.instructor)</td>
+        <td>$(taller.cupo)</td>
+        <td>$(taller.inscritos)</td>`;
 
-        const celdaNombre = document.createElement('td');
-        celdaNombre.textContent = taller.nombre;
-
-        const celdaInstructor = document.createElement('td');
-        celdaInstructor.textContent = taller.instructor;
-
-        const celdaCupo = document.createElement('td');
-        celdaCupo.textContent = taller.cupo;
-
-        const celdaInscritos = document.createElement('td');
-        celdaInscritos.textContent = taller.inscritos;
-
-        
-        fila.appendChild(celdaNombre);
-        fila.appendChild(celdaInstructor);
-        fila.appendChild(celdaCupo);
-        fila.appendChild(celdaInscritos);
-
-        tbody.appendChild(fila);
-
-    });
+        tabla.appendChild(fila); 
+    }); 
 
 }
+     
 pintarTabla(); 
 
 const formArreglos = document.getElementById('form-arreglos');
