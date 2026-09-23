2d102a0fe999902918ac0ce68546b29e1686eaf4
ruxibi <hcorreai2400@alumno.ipn.mx>
2026-09-23 00:18:52 -0600

    javascript3

--- Primer parcial/02Ejercicios/js/01-arreglos.js
+++ Primer parcial/02Ejercicios/js/01-arreglos.js
@@ -26,7 +26,18 @@ console.log(llenos.map((t)=> t.nombre))
 
 
 // TODO: find — encuentra el PRIMER taller impartido por 'Ing. María López'
+console.log("Aplicando funcion Find  en talleres")
+const tallerMaria = talleres.find((t) => t.instructor === "Ing. María López");
+console.log(tallerMaria);
 
 // TODO: reduce — calcula `totalInscritos`, la suma de inscritos de todos los talleres
+console.log("Aplicando Reduce en talleres")
+const totalInscritos = talleres.reduce((total, t) => total + t.inscritos, 0); 
+console.log(totalInscritos); 
 
 // TODO: filter + map encadenados — nombres de los talleres que SÍ tienen cupo disponible
+console.log("Aplicando Filter mas map ")
+const cdisponente = talleres
+filter((t) =>t.inscritos < t.cupo)
+.map((t) => t.nombre); 
+console.log(cdisponente); 
